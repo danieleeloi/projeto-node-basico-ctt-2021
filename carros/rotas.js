577faa20548384dcf56ad9b977a2cf8ed7e2e386
Daniele Eloi <danieleeloi@gmail.com>
2021-03-09 18:20:51 -0300

    Update rotas.js
    Acrescentado DELETE e PUT

--- carros/rotas.js
+++ carros/rotas.js
@@ -1,6 +1,6 @@
 const moment = require('moment');
 const { v4: uuidv4 } = require("uuid");
-const carros = [];
+let carros = [];
 
 app.get('/v1/carros/:id', (req, res) => {
   const { id } = req.params;
@@ -13,6 +13,48 @@ app.get('/v1/carros/:id', (req, res) => {
 app.get('/v1/carros', (req, res) => {
     return res.status(200).json(carros);
 })
+
+app.delete('/v1/carros/:id', (req, res) => {
+    const { id } = req.params;
+
+    const total = carros.length;
+    carros = carros.filter(item => id != item.id);
+
+    if (carros.length < total) {
+        return res.status(204).json({});
+    } else {
+        return res.status(404).json({mensagem: "Este carro não foi localizado!"});
+    }
+})
+
+app.put(`/v1/carros/:id`, (req, res) => {
+    const { id } = req.params;;
+    const atualizar = req.body;
+    let atualizou = false;
+    let objetoAtualizado = {};
+
+    carros = carros.map(item => {
+        if (id != item.id) {
+            return item;
+        }
+
+        atualizou = true;
+
+        objetoAtualizado = {
+            id: id,
+            ...atualizar,
+            timestamp: moment().toISOString()
+        };
+
+        return objetoAtualizado;
+    });
+
+    if (atualizou) {
+        return res.status(200).json(objetoAtualizado);
+    } else {
+        return res.status(404).json({mensagem: "Este carro não foi localizado!"});
+    }
+});
 
 app.post('/v1/carros', function (req, res) {
     const { nome, marca, tipo, placa, cor } = req.body;
